9099f79c841665f2b993724d14062fea941f7a78
Gabe Deml <gddeml@wpi.edu>
2020-12-31 03:45:45 +0000

    Basic weights working

--- Perceptron.py
+++ Perceptron.py
@@ -1,5 +1,12 @@
 class Perceptron:
-    weight = 0
+    size = 0
     bias = 0
+    weight = 0
 
-    
+    def __init__(self, size):
+        self.size = size
+        self.weight = [0 for _ in range(size)]
+
+    def Print_Weights(self):
+        print("hello")
+        print(self.weight)
--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from Perceptron import Perceptron
 data = {1,2,3,4,5}
 labels = {1,2,3,4,5}
 
-perceptron = Perceptron()
+perceptron = Perceptron(10)
 
 if __name__ == "__main__":
-    print("Place holder")
-    print(perceptron.weight)
+    perceptron.Print_Weights()
