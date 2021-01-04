8c85f330f6302647303d7aeba8f30a1208c6555a
GabrielDeml <gabrielddeml@gmail.com>
2021-01-04 16:37:08 -0500

    Put in comment the right way because VScode doesn't seem smart enough to do auto put in comments correctly.

--- FakeNumpy.py
+++ FakeNumpy.py
@@ -2,31 +2,31 @@ import os
 
 
 def random_int() -> int:
-    """ Returns a random int
-
-    Returns:
-        int: The random int generated
-
+    """
+    Creates a random number between 0 and 9
+    :return: A random int between 0 and 9
     """
     with open("/dev/urandom", 'rb') as f:
         return int(int.from_bytes(f.read(1), 'big') / 25.5)
 
 
 def get_digit(number: int, n: int) -> int:
-    """ Returns the nth number in the integer
-
-    Args:
-        number (int): Number to take the n digit from
-        n (int): location of n to take the number from
-
-    Returns:
-        int: Number taken from the orginal number
-
+    """
+    Gets the nth digit
+    :param number: Number to get the digit from
+    :param n: The location of the digit to get
+    :return: The nth digit that was gotten
     """
     return number // 10 ** n % 10
 
 
 def dot2d(matrix1: list, matrix2: list) -> int:
+    """
+    Take the dot product of the two 2d matrixes
+    :param matrix1: First matrix
+    :param matrix2: Second matrix
+    :return: Dot product of the two matrixes
+    """
     out_list = int(0)
     if len(matrix1) == len(matrix2):
         out_list = sum(
